b94cc3ed5724b02b462faba5f455b2c9cdfe55d8
Krista Francis <kristafrancis08@yahoo.com>
2022-10-28 15:13:58 -0400

    refactor css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import "./App.css";
 import React, { useState } from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
-import Footer from "./components/Footer"
+//import Projects from "./components/Projects";
 import ContactForm from "./components/Contact";
+//import Resume from "./components/Resume";
+//import Footer from "./components/Footer";
+
 
 function App() {
   const [categories] = useState([
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -33,3 +33,4 @@ function Footer() {
     </footer>
   );
 }
+export default Footer;
